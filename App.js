311dc8f8b7d040e27d76e91abdf0bd060f3afbce
NunezAndy <andy_nunez91@yahoo.com>
2024-01-12 14:32:02 -0600

    keyboard updates

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
-import './App.css';
+import "./App.css";
 import Board from "./components/Board";
-import Keyboard from './components/Keyboard';
-import { createContext, useState } from 'react';
+import Keyboard from "./components/Keyboard";
+import { createContext, useState } from "react";
 import { boardDefault } from "./components/Words";
 
 export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard }}>
+      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt }}>
         <div className="game">
-      <Board />
-      <Keyboard />
-      </div>
+          <Board />
+          <Keyboard />
+        </div>
       </AppContext.Provider>
     </div>
   );
--- components/Key.js
+++ components/Key.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../App";
 
-function Key({ keyVal }) {
+function Key({ keyVal, bigKey }) {
+    const { board, setBoard, currAttempt, setCurrAttempt } = useContext(AppContext);
+
+    const selectLetter = () => {
+        const newBoard = [...board];
+        newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+        setBoard(newBoard);
+        setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
+    };
   return (
-    <div className="key"> {keyVal} </div>
-  )
+    <div className="key" id={bigKey && "big"} onClick={selectLetter} >
+      {keyVal}
+    </div>
+  );
 }
 
-export default Key
+export default Key;
